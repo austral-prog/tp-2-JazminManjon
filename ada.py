a156a924f8357d354230998e5960a2e473f897ad
JazminManjon <jmanjonbalvidares@mail.austral.edu.ar>
2025-04-04 12:28:47 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,8 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    fullname = first_name+' '+last_name 
+    print(fullname.lower())
+    print(fullname.title())
+    print(fullname.upper())
+    print('\t'+fullname.lower())
